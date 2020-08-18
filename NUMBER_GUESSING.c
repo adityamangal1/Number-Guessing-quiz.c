e55bba06c9cf304b936e64deb6de14a338a39569
aditya mangal <adityamangal0202@gmail.com>
2020-08-18 11:34:00 +0530

    function added

--- NUMBER_GUESSING.c
+++ NUMBER_GUESSING.c
@@ -2,20 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+int number_guess(int guess, int number, int nguesses)
 {
+    printf("ENTER THE NUMBER FROM 1 TO 200\n");
 
-    int number;
-    int guess, nguesses = 1;
-    srand(time(0));
-    number = rand()%200 + 1;
-    // printf("THE NUMBER IS %d\n", number);
-
-        printf("ENTER THE NUMBER FROM 1 TO 200\n");
     do
     {
         scanf("%d", &guess);
-        if (guess > number){
+        if (guess > number)
+        {
 
             printf("LOWER NUMBER PLEASE!\n");
         }
@@ -31,6 +26,18 @@ int main()
             printf("YOU GUESSED IT IN %d attempts \n", nguesses);
         }
         nguesses++;
-    } while(guess!=number);
+    } while (guess != number);
+}
+
+int main()
+{
+
+    int number;
+    int guess, nguesses = 1;
+    srand(time(0));
+    number = rand() % 200 + 1;
+    // printf("THE NUMBER IS %d\n", number);
+    int result = number_guess(guess, number, nguesses);
+
     return 0;
 }
