5ae30aec2bb6b8b99ab9d44473dda6246243552e
aditya mangal <adityamangal0202@gmail.com>
2020-08-21 11:17:30 +0530

    function added

--- NUMBER_GUESSING.c
+++ NUMBER_GUESSING.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-int number_guess(int guess, int number, int nguesses)
+int number_guess(int number, int nguesses)
 {
-    printf("ENTER THE NUMBER FROM 1 TO 200\n");
+    int guess;
 
     do
     {
@@ -13,31 +13,42 @@ int number_guess(int guess, int number, int nguesses)
         {
 
             printf("LOWER NUMBER PLEASE!\n");
+            printf("TRY AGAIN!\n");
         }
 
         else if (guess < number)
         {
             printf("HIGHER NUMBER PLEASE!\n");
+            printf("TRY AGAIN!\n");
         }
         else
         {
 
-            printf("CONGRATULATION!\n");
+            printf("CONGRATULATION!!\n");
             printf("YOU GUESSED IT IN %d attempts \n", nguesses);
         }
         nguesses++;
     } while (guess != number);
+    return 0;
 }
 
 int main()
 {
 
     int number;
-    int guess, nguesses = 1;
+    int nguesses = 1;
+    char o;
+    int num, num1;
     srand(time(0));
     number = rand() % 200 + 1;
-    // printf("THE NUMBER IS %d\n", number);
-    int result = number_guess(guess, number, nguesses);
+    // printf("THE NUMBER IS %d\n", number
+    printf("\t\t\t****WELCOME TO NUMBER GUESSING GAME****\t\t\t\t\n");
+    printf("\t\t\tHOPE YOU ENJOY!!\t\t\t\t\n");
+    printf("ENTER ANY NUMBER BETWEEN 1 TO 200 :-\n");
+    number_guess(number, nguesses);
+    printf("\t\t\t*****GAME OVER*****\t\t\t\n");
+    printf("THANK YOU! FOR PLAYING.\n");
+    printf("COME SOON AGAIN!!\n");
 
     return 0;
 }
